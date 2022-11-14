f0fdbfa8190454b824db57c3cf4e45148e860eac
txxxxc <tomoya113.code@gmail.com>
2022-11-14 17:50:20 +0900

    [update] nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
     },
   ],
   css: [
+    '@/assets/styles/main.scss',
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css',
   ],
