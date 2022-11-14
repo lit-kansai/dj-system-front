5729fa5623b2df7d20a4b5d3e299e9e9e1c9f039
txxxxc <tomoya113.code@gmail.com>
2022-11-14 17:50:09 +0900

    [update] default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,27 @@
 <template>
   <main id="app-layout">
-    <slot />
+    <v-layout>
+      <v-navigation-drawer permanent></v-navigation-drawer>
+      <v-app-bar id="app-bar" color="grey-lighten-4">
+        <template #prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+      </v-app-bar>
+      <v-main>
+        <slot />
+      </v-main>
+    </v-layout>
   </main>
 </template>
 <script setup lang="ts"></script>
-<style scoped>
+<style scoped lang="scss">
+  @import '@/assets/styles/main.scss';
   #app-layout {
-    height: 100vh;
+    height: 100%;
     min-height: 100vh;
+  }
+
+  #app-bar {
+    height: $app-bar-height;
   }
 </style>
