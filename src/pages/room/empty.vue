e3a460cf9822632c22fbd484213156dcfd45563b
Yo Higashida <y.higashida0226+app@gmail.com>
2022-11-22 18:18:45 +0900

    add: empty.vue

---
+++ src/pages/room/empty.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><span class="material-icons">double_arrow</span></div>
+</template>
+
+<script setup lang="ts">
+  definePageMeta({})
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/styles/main.scss';
+</style>
