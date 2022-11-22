59a636ede0d05df1e6e7eecf390ddbd6dd5e7579
Yo Higashida <y.higashida0226+app@gmail.com>
2022-11-22 18:44:22 +0900

    add: empty

--- src/pages/room/empty.vue
+++ src/pages/room/empty.vue
@@ -1,5 +1,15 @@
 <template>
-  <div><span class="material-icons">double_arrow</span></div>
+  <div class="v-container h-full">
+    <div class="d-flex flex-column justify-center align-center">
+      <v-icon size="180" class="mb-4">mdi-rocket</v-icon>
+      <p class="text-h5 mb-1 font-weight-black">まだルームがありません</p>
+      <p class="mb-3">
+        外部サービス（AppleMusic /
+        Spotify）を接続した後に、ルームを作成しましょう。
+      </p>
+      <v-btn color="blue-darken-3" elevation="2">ルームを追加する</v-btn>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
