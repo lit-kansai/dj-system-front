9f8d767c5aa73d4a2b788dd67e9f9127c405ab2d
txxxxc <tomoya113.code@gmail.com>
2022-12-25 02:43:42 +0900

    [update] create-post.ts

--- src/features/post/api/create-post.ts
+++ src/features/post/api/create-post.ts
@@ -1,25 +1,35 @@
 // NOTE: sample
-import { _AsyncData } from 'nuxt/dist/app/composables/asyncData'
-import { ApiInstance } from '@/api/$api'
-import { apiClient } from '@/libs'
+import { Ref } from 'nuxt/dist/app/compat/capi'
+import {
+  sampleApiClient,
+  ApiInstance as APIInstance,
+  APIRequestOutput,
+} from '@/libs'
+import { Post } from '@/features/post'
 
+// NOTE: ここガチでクソ複雑だけどしゃーないです(補完あるから耐えてはいる)
+// というかaspida側で良い感じに書けるようにしてほしい笑
+// こっちでUtility用意するのもあり(がんばります)
 export type CreatePostInput = Parameters<
-  ApiInstance['api']['posts']['$post']
+  APIInstance['api']['posts']['$post']
 >[0]['body']
+export type CreatePostOutput = APIRequestOutput<Post>['post']
 
-export type Post = Awaited<ReturnType<ApiInstance['api']['posts']['$post']>>
-
-// export type CreatePostOutput = ReturnType<typeof useAsyncData<Post>>
-export type CreatePostOutput = Promise<_AsyncData<Post, true | Error | null>>
-
-export const createPost = async (body: CreatePostInput): CreatePostOutput => {
-  const response = await useAsyncData<Post>(
-    () => apiClient.api.posts.$post({ body }),
-    {
-      initialCache: false,
-      immediate: false,
-      watch: [],
+export const createPost = (body: CreatePostInput): CreatePostOutput => {
+  const pending: Ref<boolean> = ref(false)
+  const data: Ref<Post | null> = ref(null)
+  const error: Ref<true | Error | null> = ref(null)
+  const execute: () => Promise<void> = async () => {
+    try {
+      pending.value = true
+      const response = await sampleApiClient.api.posts.$post({ body })
+      data.value = response
+      pending.value = false
+    } catch (_error) {
+      pending.value = false
+      _error instanceof Error ? (error.value = _error) : (error.value = true)
     }
-  )
-  return response
+  }
+
+  return { pending, data, error, execute }
 }
