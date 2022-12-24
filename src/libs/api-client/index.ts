5d8e593e14c8a54fe01259af0bd8f6343e2a5daf
txxxxc <tomoya113.code@gmail.com>
2022-12-25 02:45:24 +0900

    [update] api-client/index.ts

--- src/libs/api-client/index.ts
+++ src/libs/api-client/index.ts
@@ -1,10 +1,1 @@
-import aspida from '@aspida/axios'
-import axios from 'axios'
-import api from '@/api/$api'
-
-export const apiClient = api(
-  aspida(axios, {
-    timeout: 3000,
-    baseURL: 'https://jsonplaceholder.typicode.com',
-  })
-)
+export * from './types'
