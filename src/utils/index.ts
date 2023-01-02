7a1af02b6525c2b66a4a0a11acd5db9538111caa
txxxxc <tomoya113.code@gmail.com>
2023-01-03 02:59:20 +0900

    [update] utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
-export * from "./camel-to-snake";
-export * from "./snake-to-camel";
-export * from "./token-required";
+export * from './camel-to-snake'
+export * from './snake-to-camel'
+export * from './token-required'
+export * from './is-record'
