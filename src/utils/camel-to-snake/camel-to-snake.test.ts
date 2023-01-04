8122aced96248fc0b764eee6445f703c611a3638
txxxxc <tomoya113.code@gmail.com>
2023-01-04 15:42:18 +0900

    [update] camel-to-snake.ts

--- src/utils/camel-to-snake/camel-to-snake.test.ts
+++ src/utils/camel-to-snake/camel-to-snake.test.ts
@@ -11,21 +11,29 @@ describe('snake-to-camel', () => {
   })
 
   it('ネストするやつ', () => {
-    expect(camelToSnake({
-      name: {
-        firstName: 'name'
-      }
-    })).toEqual({
-      name: {
-        first_name: 'name'
-      }
-    })
+    expect(camelToSnake({ name: { firstName: 'name' } }))
+      .toEqual({ name: { first_name: 'name' } })
   })
 
   it('ネストした先に配列があるやつ', () => {
-    expect(camelToSnake(
-      { names: [{ firstName: 'hoge' }] }
-    )).toEqual(
-      { names: [{ first_name: 'hoge' }] })
+    expect(camelToSnake({ names: [{ firstName: 'hoge' }] }))
+      .toEqual({ names: [{ first_name: 'hoge' }] })
+  })
+
+  it('二重配列', () => {
+    expect(camelToSnake([[{ firstName: 'name' }]]))
+      .toEqual([[{ first_name: 'name' }]])
+  })
+
+  it('null', () => {
+    expect(camelToSnake(null)).toEqual(null)
+  })
+
+  it('{}', () => {
+    expect(camelToSnake({})).toEqual({})
+  })
+
+  it('プリミティブ', () => {
+    expect(camelToSnake('name')).toEqual('name')
   })
 })
