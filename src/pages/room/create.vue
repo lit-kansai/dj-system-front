bfa77576e473f8f9963dd5f1946d26470af025b8
Yo Higashida <y.higashida0226+app@gmail.com>
2023-01-06 16:37:27 +0900

    Revert "add: v-model"
    This reverts commit c1b7131133f46c77bb6b44761fc695dfebc16351.

--- src/pages/room/create.vue
+++ src/pages/room/create.vue
@@ -4,21 +4,19 @@
       ルームを作成する
     </p>
     <q-input
-      v-model="form.roomName"
       outlined
       label="ルームネーム*"
       :rules="[(val) => !!val || 'Field is required']"
     />
-    <q-input v-model="form.roomDescription" outlined label="ルーム説明" class="not-rule-input" />
+    <q-input outlined label="ルーム説明" class="not-rule-input" />
     <q-select
-      v-model="form.provider"
+      v-model="provider"
       outlined
       :options="providerOptions"
       label="外部サービス*"
       class="not-rule-input"
     />
     <q-input
-      v-model="form.requestUrl"
       outlined
       label="リクエストURL*"
       class="url-prefix"
@@ -32,12 +30,6 @@
 </template>
 
 <script setup lang="ts">
-  const form = reactive({
-    roomName: '',
-    roomDescription: '',
-    provider: '',
-    requestUrl: ''
-  })
   const providerOptions = ['Spotify', 'AppleMusic']
 </script>
 
