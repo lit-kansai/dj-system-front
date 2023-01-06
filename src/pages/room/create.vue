72b093b8f7e262e849169880183dc179be6a1cc1
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2023-01-06 18:20:09 +0900

    /room/create で使う値を変数管理にする (#85)

--- src/pages/room/create.vue
+++ src/pages/room/create.vue
@@ -4,33 +4,47 @@
       ルームを作成する
     </p>
     <q-input
+      v-model="form.roomName"
       outlined
       label="ルームネーム*"
       :rules="[(val) => !!val || 'Field is required']"
     />
-    <q-input outlined label="ルーム説明" class="not-rule-input" />
+    <q-input v-model="form.roomDescription" outlined label="ルーム説明" class="not-rule-input" />
     <q-select
-      v-model="provider"
+      v-model="form.provider"
       outlined
       :options="providerOptions"
       label="外部サービス*"
       class="not-rule-input"
     />
     <q-input
+      v-model="form.requestUrl"
       outlined
       label="リクエストURL*"
       class="url-prefix"
       prefix="https://dj.life-is-tech.com/"
       :rules="[(val) => !!val || 'Field is required']"
     />
-    <div class="column items-end">
+    <div class="row justify-end items-end">
+      <q-btn color="grey-1" text-color="dark" class="q-mr-sm" label="キャンセル" @click="onClickCancel" />
       <q-btn color="primary" label="ルームを作成する" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  const router = useRouter()
+  const form = reactive({
+    roomName: '',
+    roomDescription: '',
+    provider: '',
+    requestUrl: ''
+  })
   const providerOptions = ['Spotify', 'AppleMusic']
+
+  const onClickCancel = () => {
+    router.go(-1)
+  }
 </script>
 
 <style lang="scss" scoped>
