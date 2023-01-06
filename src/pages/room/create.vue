c1b7131133f46c77bb6b44761fc695dfebc16351
Yo Higashida <y.higashida0226+app@gmail.com>
2023-01-06 15:55:54 +0900

    add: v-model

--- src/pages/room/create.vue
+++ src/pages/room/create.vue
@@ -4,19 +4,21 @@
       ルームを作成する
     </p>
     <q-input
+      v-model="form.roomName"
       outlined
       label="ルームネーム*"
       :rules="[(val) => !!val || 'Field is required']"
     />
-    <q-input outlined label="ルーム説明" class="not-rule-input" />
+    <q-input v-model="form.roomDescription" outlined label="ルーム説明" class="not-rule-input" />
     <q-select
-      v-model="provider"
+      v-model="form.provider"
       outlined
       :options="providerOptions"
       label="外部サービス*"
       class="not-rule-input"
     />
     <q-input
+      v-model="form.requestUrl"
       outlined
       label="リクエストURL*"
       class="url-prefix"
@@ -30,6 +32,12 @@
 </template>
 
 <script setup lang="ts">
+  const form = reactive({
+    roomName: '',
+    roomDescription: '',
+    provider: '',
+    requestUrl: ''
+  })
   const providerOptions = ['Spotify', 'AppleMusic']
 </script>
 
