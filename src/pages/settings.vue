101ff1d9642db3094c224165923fab8e22c5a3ed
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2023-01-06 18:19:51 +0900

    /settings で使用する値を変数管理にする (#84)

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -12,7 +12,17 @@
     <p class="q-mb-sm">
       AppleMusicのサブスクへの登録が必要になります。
     </p>
-    <q-btn color="primary" class="q-mb-lg" label="AppleMusicにログイン" />
+    <q-btn v-if="!linkedProviders.applemusic" color="primary" class="q-mb-lg" no-caps label="AppleMusicにログイン" />
+    <q-btn
+      v-else
+      color="grey-4"
+      text-color="dark"
+      icon="link"
+      class="q-mb-lg"
+      no-caps
+      disable
+      label="AppleMusicと連携済み"
+    />
 
     <p class="text-h6 text-weight-bold q-mb-xs">
       Spotify
@@ -20,8 +30,23 @@
     <p class="q-mb-sm">
       APIの利用制限のため、選ばれし25名のみが使用できます。がっしーにご連絡ください。
     </p>
-    <q-btn color="primary" class="q-mb-lg" label="Spotifyにログイン" />
+    <q-btn v-if="!linkedProviders.spotify" color="primary" class="q-mb-lg" no-caps label="Spotifyにログイン" />
+    <q-btn
+      v-else
+      color="grey-4"
+      text-color="dark"
+      icon="link"
+      class="q-mb-lg"
+      no-caps
+      disable
+      label="Spotifyと連携済み"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const linkedProviders = reactive({
+    applemusic: false,
+    spotify: true
+  })
+</script>
