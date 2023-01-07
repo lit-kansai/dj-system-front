0192e5d142c0c0bd8aacb2fa72eec90aaf18d561
txxxxc <tomoya113.code@gmail.com>
2023-01-07 12:58:31 +0900

    [generate] api client by using aspida #89

---
+++ src/libs/api/generated/mc/user/link/spotify/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import type * as Types from '../../../../@types'
+
+export type Methods = {
+  get: {
+    query: Types.Spotify_redirect_url
+    status: 200
+
+    /** Success */
+    resBody: {
+      redirect_url: Types.Login_url
+    }
+  }
+}
---
+++ src/libs/api/generated/room/_roomId@string/music/search/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import type * as Types from '../../../../@types'
+
+export type Methods = {
+  get: {
+    query: Types.Query
+    status: 200
+    /** 音楽情報の配列 */
+    resBody: Types.Track[]
+  }
+}
