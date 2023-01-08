b70f05ae76c1e75e9791a8b0d252efea55102774
txxxxc <tomoya113.code@gmail.com>
2023-01-08 12:51:02 +0900

    [update] auth/api/signin #78

--- src/features/auth/api/signin.ts
+++ src/features/auth/api/signin.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { apiClient } from '@/libs/api'
+import { apiClient, ApiInstance } from '@/libs/api'
 import { CamelizedAPIResponse, toSchema } from '@/types'
 import { GetRequestOutput } from '@/libs'
 
@@ -7,7 +7,7 @@ import { GetRequestOutput } from '@/libs'
 export type SigninInput = { redirectUrl: string }
 
 // 2. aspidaが返す値をキャメルケースに変換する
-export type SinginResponse = CamelizedAPIResponse<typeof apiClient.mc.auth.signin.$get>
+export type SinginResponse = CamelizedAPIResponse<ApiInstance['mc']['auth']['signin']['$get']>
 
 // 3. 2で作った型からzodのschemaを作る
 const responseSchema = toSchema<SinginResponse>()(
@@ -20,7 +20,7 @@ export const signin = async (input: SigninInput): GetRequestOutput<SinginRespons
   // 4. NuxtのuseLazyAsyncDataでaspidaをラップする
   const result = await useLazyAsyncData(async () => {
     const response = await apiClient().mc.auth.signin.$get({
-      query: { redirect_url: input.redirectUrl }
+      query: { redirect_url: window.location.origin + input.redirectUrl }
     })
 
     // 5. 返ってくる値をキャメルケースに変換する
