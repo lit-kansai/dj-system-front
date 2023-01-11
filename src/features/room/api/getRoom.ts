043c31eb34045f240eb247f63efb3590fc92a000
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2023-01-11 18:41:47 +0900

    Room情報表示のAPIを叩く関数 (#109)

---
+++ src/features/room/api/getRoom.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+import { apiClient, ApiInstance } from '@/libs/api'
+import { CamelizedAPIResponse, toSchema } from '@/types'
+import { GetRequestOutput } from '@/libs'
+
+export type getRoomInput = { roomId: string }
+
+export type getRoomResponse = CamelizedAPIResponse<ReturnType<ApiInstance['room']['_roomId']>['$get']>
+
+const responseSchema = toSchema<getRoomResponse>()(
+  z.object({
+    id: z.string(),
+    name: z.string(),
+    description: z.string()
+  })
+)
+
+export const getRoom = async (input: getRoomInput): GetRequestOutput<getRoomResponse> => {
+  const result = await useLazyAsyncData(async () => {
+    const response = await apiClient().room._roomId(input.roomId).$get()
+
+    const parseResult = responseSchema.safeParse(response)
+    if (!parseResult.success) {
+      const { setCurrentError } = useCurrentError()
+      setCurrentError(parseResult.error)
+      throw parseResult.error
+    }
+    return parseResult.data
+  })
+  return result
+}
