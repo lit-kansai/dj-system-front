22a79a2af31c8eae3413d1bd86074691a2167b7a
txxxxc <tomoya113.code@gmail.com>
2023-01-18 14:46:33 +0900

    [add] constants/http #120

---
+++ src/constants/http.ts
@@ -0,0 +1,4 @@
+export const COOKIE_AUTHORIZATION_KEY = 'authorization' as const
+export const AUTHRIZATION_HEADER_FIELD = 'api-token' as const
+export const CONTENT_TYPE_KEY = 'Content-Type' as const
+export const CONTENT_TYPE_VALUE = 'application/json; charset=UTF-8' as const
