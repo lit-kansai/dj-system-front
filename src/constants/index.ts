969ad5822716a3d9b710573c898713a018037532
txxxxc <tomoya113.code@gmail.com>
2023-01-18 14:47:03 +0900

    [update] constants/index #120

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,2 @@
-export const COOKIE_AUTHORIZATION_KEY = 'authorization' as const
-export const AUTHRIZATION_HEADER_FIELD = 'api-token' as const
-export const LOGIN_PAGE = '/login' as const
-export const CONTENT_TYPE_KEY = 'Content-Type' as const
-export const CONTENT_TYPE_VALUE = 'application/json; charset=UTF-8' as const
 export * from './endpoints'
+export * from './http'
