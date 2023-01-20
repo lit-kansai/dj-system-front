4bcbc6f7712826076d68f1e609b57752cc81e48f
txxxxc <tomoya113.code@gmail.com>
2023-01-20 17:36:32 +0900

    [rename] registerUser to register-user

--- src/features/auth/api/register-user.ts
+++ src/features/auth/api/register-user.ts
@@ -3,16 +3,13 @@ import { apiClient, ApiInstance } from '@/libs/api'
 import { CamelizedAPIResponse, toSchema } from '@/types'
 import { GetRequestOutput } from '@/libs'
 
-// 1. request body か query parametersを受け取るinput
 export type RegisterUserInput = {
   code: string
   redirectUrl: string
 }
 
-// 2. aspidaが返す値をキャメルケースに変換する
 export type RegisterUserResponse = CamelizedAPIResponse<ApiInstance['mc']['auth']['signin']['callback']['$post']>
 
-// 3. 2で作った型からzodのschemaを作る
 const responseSchema = toSchema<RegisterUserResponse>()(
   z.object({
     apiToken: z.string(),
@@ -21,7 +18,6 @@ const responseSchema = toSchema<RegisterUserResponse>()(
 )
 
 export const registerUser = async (input: RegisterUserInput): GetRequestOutput<RegisterUserResponse> => {
-  // 4. NuxtのuseLazyAsyncDataでaspidaをラップする
   const result = await useLazyAsyncData(async () => {
     const response = await apiClient().mc.auth.signin.callback.$post({
       body: {
@@ -29,7 +25,7 @@ export const registerUser = async (input: RegisterUserInput): GetRequestOutput<R
         redirect_url: input.redirectUrl
       }
     })
-    // 5. 返ってくる値をキャメルケースに変換する
+
     const parseResult = responseSchema.safeParse(response)
     if (!parseResult.success) {
       const { setCurrentError } = useCurrentError()
