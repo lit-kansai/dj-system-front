1246d4d682343216dd5b9dc52a2b2c565ac5693d
txxxxc <tomoya113.code@gmail.com>
2023-01-20 17:36:05 +0900

    [add] local-storage key constant #98

--- src/libs/api/token-fetcher/local-storage.ts
+++ src/libs/api/token-fetcher/local-storage.ts
@@ -1,12 +1,18 @@
+import { z } from 'zod'
 import { TokenFetcher } from '../interceptors'
+import { USER_INFO } from '@/constants'
+
+const localStorageSchema = z.object({
+  apiToken: z.string()
+})
 
 export const local: TokenFetcher = {
   fetch: () => {
-    const data = localStorage.getItem('userInfo')
-    if (!data) {
-      return null
-    }
-    const { token } = JSON.parse(data) as { token: string }
-    return token || null
+    const userInfo = localStorage.getItem(USER_INFO)
+    if (!userInfo) { return null }
+    const data = JSON.parse(userInfo)
+    const result = localStorageSchema.safeParse(data)
+    if (!result.success) { return null }
+    return result.data.apiToken
   },
 }
