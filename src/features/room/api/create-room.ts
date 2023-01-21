8f0b9f2dfe4f8d4029ed5148e52de3ee7c7df181
Yo Higashida <y.higashida0226+app@gmail.com>
2023-01-21 21:22:48 +0900

    add: create-room

---
+++ src/features/room/api/create-room.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod'
+import { apiClient, ApiInstance } from '@/libs/api'
+import { CamelizedAPIResponse, toSchema } from '@/types'
+import { GetRequestOutput } from '@/libs'
+
+export type createRoomInput = {
+  id: string,
+  urlName: string,
+  roomName: string,
+  description: string,
+  provider: string,
+  playlistId: string | undefined
+}
+
+export type createRoomResponse = CamelizedAPIResponse<ApiInstance['mc']['room']['$post']>
+
+const responseSchema = toSchema<createRoomResponse>()(
+  z.object({
+    id: z.string(),
+    urlName: z.string(),
+    roomName: z.string(),
+    description: z.string(),
+    provider: z.string(),
+    playlistId: z.string(),
+  })
+)
+
+export const createRoom = async (input: createRoomInput): GetRequestOutput<createRoomResponse> => {
+  const result = await useLazyAsyncData(async () => {
+    const response = await apiClient().mc.room.$post({
+      body: {
+        url_name: input.urlName,
+        room_name: input.roomName,
+        description: input.description,
+        provider: input.provider,
+        playlist_id: input.playlistId ?? undefined
+      }
+    })
+
+    const parseResult = responseSchema.safeParse(response)
+    if (!parseResult.success) {
+      const { setCurrentError } = useCurrentError()
+      setCurrentError(parseResult.error)
+      throw parseResult.error
+    }
+    return parseResult.data
+  }, {
+    immediate: false
+  })
+  return result
+}
