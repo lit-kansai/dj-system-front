3d4cf3d71e5d7eb54ceb1fd36ce2fb3fbbe77d8c
txxxxc <tomoya113.code@gmail.com>
2023-01-22 09:49:56 +0900

    [update] nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,7 +55,7 @@ export default defineNuxtConfig({
   },
   hooks: {
     'prepare:types': ({ tsConfig }) => {
-      const aliasesToRemoveFromAutocomplete = ['~~', '~~/*', '~']
+      const aliasesToRemoveFromAutocomplete = ['~~', '~~/*', '~', '@@']
       for (const alias of aliasesToRemoveFromAutocomplete) {
         if (tsConfig.compilerOptions?.paths[alias]) {
           delete tsConfig.compilerOptions.paths[alias]
