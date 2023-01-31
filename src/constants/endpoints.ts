37e910cda30de4ed9cd2487ad802879ed75a16e4
txxxxc <tomoya113.code@gmail.com>
2023-01-31 23:03:43 +0900

    [update] endpoints constants

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -3,5 +3,7 @@ export const EXCEPTION_ENDPOINTS = ['/mc/auth/signin', '/mc/auth/signin/callback
 export const GOOGLE_API_CALLBACK_PATH = '/api/google/callback' as const
 export const GOOGLE_API_CALLBACK_URL = `${window.origin}${GOOGLE_API_CALLBACK_PATH}`
 export const SPOTIFY_API_CALLBACK_PATH = '/api/spotify/callback' as const
+export const SPOTIFY_API_CALLBACK_URL = `${window.origin}${SPOTIFY_API_CALLBACK_PATH}`
 export const APPLE_MUSIC_API_CALLBACK_PATH = '/api/apple/callback' as const
 export const LOGIN_PAGE = '/login' as const
+export const SETTINGS_PAGE = '/settings' as const
