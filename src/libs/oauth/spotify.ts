07eb4fd2b9eee480c87c008feda2ffad5e8cae57
txxxxc <tomoya113.code@gmail.com>
2023-01-31 23:04:53 +0900

    [add] oauth spotify #145

---
+++ src/libs/oauth/spotify.ts
@@ -0,0 +1,12 @@
+import { logger, spotifyOAuthQuerySchema } from '@/libs'
+import { LocationQuery } from '@/types'
+import { integrateSpotify } from '@/features/auth/api/integate-spotify'
+
+export const spotify = async (input: {query: LocationQuery}) => {
+  // TODO: エラーハンドリングしよう
+  const parseResult = spotifyOAuthQuerySchema.safeParse(input.query)
+  if (!parseResult.success) { logger.log('invalid parameters', input.query); return }
+  const { code } = parseResult.data
+  const { data } = await integrateSpotify({ code })
+  return data.value
+}
