00ef9b47edb455e59a6bb4eac2325d6a9ca1b72e
Tomoya Tanaka <tt.ios.develop@gmail.com>
2023-02-04 21:02:33 +0900

    [fix] create-room response error (#157)

--- src/types/camelized-api-response.ts
+++ src/types/camelized-api-response.ts
@@ -3,8 +3,7 @@ import { SnakeToCamel } from './snake-to-camel'
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type CamelizedAPIResponse<T extends (...args: any) => any> =
   Awaited<ReturnType<T>> extends any[] ?
-    Array<SnakeToCamel<Awaited<ReturnType<T>>[number]>>
-    :
+    Array<SnakeToCamel<Awaited<ReturnType<T>>[number]>> :
     SnakeToCamel<Awaited<ReturnType<T>>>
 
 export type _CamelizedAPIResponse<T extends object | any[]> = SnakeToCamel<T>
