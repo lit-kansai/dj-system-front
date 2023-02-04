dd28125244bfd0a365106e39cf0bd8a6c0079817
Yo Higashida <y.higashida0226+app@gmail.com>
2023-02-04 17:08:18 +0900

    add: user-type

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export type User = {
+  id: number
+  google_id: string
+  email: string
+  name: string
+  icon: string
+  is_admin: boolean
+  created_at: string
+  updated_at: string
+  linked_providers?: Array<{provider: string, is_connected: boolean}> | undefined
+}
