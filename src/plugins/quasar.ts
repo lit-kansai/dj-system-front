e208ecf659fc292bf5c68f733e1e1524bab1a611
Tomoya Tanaka <tt.ios.develop@gmail.com>
2023-02-04 18:31:03 +0900

    Feature/#139 create room (#154)
    * [update] create-room
    
    * [update] pages/room/create.vue
    
    * [add] plugins/quasar

--- src/plugins/quasar.ts
+++ src/plugins/quasar.ts
@@ -1,9 +1,11 @@
-import { Quasar } from 'quasar'
+import { Quasar, Loading } from 'quasar'
 import * as components from 'quasar'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(Quasar, {
-    plugins: {},
+    plugins: {
+      Loading
+    },
     components,
   })
 })
