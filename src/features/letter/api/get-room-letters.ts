a6602ecca2a006e43dc68efbe4447127a32f5b90
txxxxc <tomoya113.code@gmail.com>
2023-02-12 01:29:30 +0900

    [add] letter features

--- src/features/letter/api/get-room-letters.ts
+++ src/features/letter/api/get-room-letters.ts
@@ -1,36 +1,57 @@
 import { z } from 'zod'
-import { apiClient, ApiInstance } from '@/libs/api'
-import { CamelizedAPIResponse, toSchema } from '@/types'
+import { Ref } from 'nuxt/dist/app/compat/capi'
+import { apiClient } from '@/libs/api'
+import { _CamelizedAPIResponse } from '@/types'
 import { GetRequestOutput } from '@/libs'
 
-export type GetRoomLettersInput = { roomId: string }
+export type GetRoomLettersInput = { roomId: Ref<string> }
 
-export type GetRoomLettersResponse = CamelizedAPIResponse<ReturnType<ApiInstance['mc']['room']['_roomId']>['letters']['$get']>
-
-const responseSchema = toSchema<GetRoomLettersResponse>()(
-  z.object({
-    id: z.string(),
-    roomId: z.number(),
-    radioName: z.string(),
-    message: z.string(),
-    createdAt: z.string(),
-    updatedAt: z.string(),
-    musics: z.object({
-      id: z.number(),
-      providedMusicId: z.string(),
-      letterId: z.string(),
-      artists: z.string(),
-      album: z.string(),
-      name: z.string(),
-      thumbnail: z.string(),
-      duration: z.string()
-    }).array()
+const responseSchema = z.object({
+  id: z.number(),
+  roomId: z.number(),
+  radioName: z.string(),
+  message: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  musics: z.object({
+    id: z.number(),
+    providedMusicId: z.string(),
+    letterId: z.number(),
+    artists: z.union([z.string(), z.undefined()]),
+    album: z.string(),
+    name: z.string(),
+    thumbnail: z.string(),
+    duration: z.number()
   }).array()
-)
+}).array()
+
+export type GetRoomLettersResponse = _CamelizedAPIResponse<z.infer<typeof responseSchema>>
+
+// NOTE: Swaggerと実際の方が違うので保留
+// const responseSchema = toSchema<Partial<GetRoomLettersResponse>>()(
+//   z.object({
+//     id: z.string(),
+//     roomId: z.number(),
+//     radioName: z.string(),
+//     message: z.string(),
+//     createdAt: z.string(),
+//     updatedAt: z.string(),
+//     musics: z.object({
+//       id: z.number(),
+//       providedMusicId: z.string(),
+//       letterId: z.string(),
+//       artists: z.string(),
+//       album: z.string(),
+//       name: z.string(),
+//       thumbnail: z.string(),
+//       duration: z.string()
+//     }).array()
+//   }).array()
+// )
 
 export const getRoomLetters = async (input: GetRoomLettersInput): GetRequestOutput<GetRoomLettersResponse> => {
   const result = await useLazyAsyncData(async () => {
-    const response = await apiClient().mc.room._roomId(input.roomId).letters.$get()
+    const response = await apiClient().mc.room._roomId(input.roomId.value).letters.$get()
 
     const parseResult = responseSchema.safeParse(response)
     if (!parseResult.success) {
@@ -39,6 +60,7 @@ export const getRoomLetters = async (input: GetRoomLettersInput): GetRequestOutp
       throw parseResult.error
     }
     return parseResult.data
-  })
+  }, { immediate: false })
+  result.pending.value = false
   return result
 }
