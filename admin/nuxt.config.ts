24404bfd030ec09b95abc1b4f382467d8a00f646
txxxxc <tomoya113.code@gmail.com>
2023-03-11 14:31:25 +0900

    [update] nuxt.config.ts

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -39,6 +39,7 @@ export default defineNuxtConfig({
     '@quasar/extras/roboto-font/roboto-font.css',
     '@quasar/extras/material-icons/material-icons.css',
     '@quasar/extras/fontawesome-v6/fontawesome-v6.css',
+    '@quasar/extras/fontawesome-v5/fontawesome-v5.css',
     '@mdi/font/css/materialdesignicons.min.css',
   ],
   srcDir: 'src/',
