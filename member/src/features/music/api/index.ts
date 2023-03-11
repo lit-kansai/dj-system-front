02d8ccc87f09cf0e79f1183cc464f744be99bd69
txxxxc <tomoya113.code@gmail.com>
2023-03-11 20:01:48 +0900

    [add] music apis

---
+++ member/src/features/music/api/index.ts
@@ -0,0 +1,3 @@
+export * from './search-musics'
+export * from './request-music'
+export * from './get-top50-musics'
---
+++ member/src/features/music/api/search-musics.ts
@@ -0,0 +1,43 @@
+import { z } from 'zod'
+import { CamelizedAPIResponse, toSchema, _CamelizedAPIResponse } from '@dj-system/utils'
+import { apiClient, ApiInstance, PostRequestOutput } from '@/libs'
+
+export type SearchMusicResponse = CamelizedAPIResponse<ReturnType<ApiInstance['room']['_roomId']>['music']['search']['$get']>
+
+export type SearchMusicInput = {
+  roomId: string,
+  query: string
+}
+
+const responseSchema = toSchema<SearchMusicResponse>()(
+  z.array(
+    z.object({
+      id: z.string(),
+      artists: z.string(),
+      album: z.string(),
+      name: z.string(),
+      thumbnail: z.string(),
+      duration: z.string()
+    })
+  )
+)
+export const requestMusic = (input: SearchMusicInput): PostRequestOutput<SearchMusicResponse> => {
+  const result = useLazyAsyncData(async () => {
+    const response = await apiClient().room._roomId(input.roomId).music.search.$get({
+      // HELP: ここ型つかない？(Swagger頑張ればワンチャンある？)
+      query: { q: input.query }
+    })
+    const parseResult = responseSchema.safeParse(response)
+    if (!parseResult.success) {
+      // const { setCurrentError } = useCurrentError()
+      // setCurrentError(parseResult.error)
+      // throw parseResult.error
+      throw parseResult.error
+    }
+    return parseResult.data
+  }, {
+    immediate: false
+  })
+  result.pending.value = false
+  return result
+}
