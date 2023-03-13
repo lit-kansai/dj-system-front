859339b4b20c7b0384d4add9cf8671e05752df3d
Tomoya Tanaka <tt.ios.develop@gmail.com>
2023-03-14 03:10:39 +0900

    Feature/#200 assets (#201)
    * [add] scss
    
    * [update] nuxt.config.ts
    
    * [add] global.scss
    
    * [add] variables.scss
    
    * [add] img

--- member/nuxt.config.ts
+++ member/nuxt.config.ts
@@ -7,18 +7,34 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'DJ System Admin' },
+        { name: 'description', content: 'DJ System Member' },
         { name: 'robots', content: 'noindex' },
-        { property: 'og:site_name', content: 'DJ System Admin' },
+        { property: 'og:site_name', content: 'DJ System Member' },
         { property: 'og:type', content: 'website' },
-        { property: 'og:description', content: 'DJ Systemの管理画面サイト' },
+        { property: 'og:description', content: 'DJ System' },
         { property: 'og:title', content: 'DJ System' },
         { property: 'og:url', content: baseUrl },
         { property: 'og:image', content: `${baseUrl}/ogp.png` },
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:site', content: 'DJ System' },
       ],
-    }
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+        },
+      ]
+    },
   },
   ssr: true,
   dev: process.env.NODE_ENV !== 'production',
@@ -50,5 +66,14 @@ export default defineNuxtConfig({
         }
       }
     }
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/css/variables.scss";',
+        },
+      },
+    },
   },
 })
