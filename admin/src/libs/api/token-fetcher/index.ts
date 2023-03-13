62028adabc0861c828ba91875e8b4d46cbc80f43
txxxxc <tomoya113.code@gmail.com>
2023-03-14 01:55:23 +0900

    [update] token-fetcher

--- admin/src/libs/api/token-fetcher/index.ts
+++ admin/src/libs/api/token-fetcher/index.ts
@@ -1,6 +1,11 @@
 import { cookie } from './cookie'
 import { local } from './local-storage'
+import { isDev } from '@/utils'
 export const tokenFetcher = {
   cookie,
   local,
+  fetch: (): string | null => {
+    if (isDev) { return local.fetch() }
+    return cookie.fetch()
+  }
 }
