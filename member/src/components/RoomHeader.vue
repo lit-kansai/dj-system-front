54088c989a0e97e4c3f3852ae8e540387a0ecefa
Tomoya Tanaka <tt.ios.develop@gmail.com>
2023-03-15 02:19:29 +0900

    Feature/#193 member header (#207)
    * [add] logo.svg
    
    * [update] variables.scss
    
    * [add] TextInput.vue
    
    * [add] RoomHeader.vue

---
+++ member/src/components/RoomHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <img width="60px" src="img/logo.svg">
+    <p class="room_name">ルーム名</p>
+    <TextInput />
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    padding: 16px 48px 16px 48px;
+    box-shadow: 0px 3px 6px $shadow-color;
+    background: #ffffff;
+    .room_name {
+      font-size: 20px;
+      color: $text-color-gray;
+      margin-left: 16px;
+      flex-grow: 1;
+    }
+  }
+</style>
