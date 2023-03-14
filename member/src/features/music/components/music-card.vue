78834a93d20bac80bef65f0da342329c0bba8390
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2023-03-15 02:31:47 +0900

    Feature/member/music card (#206)
    * add: img-vertical-align
    
    * add: box-shadow
    
    * add: music-card

---
+++ member/src/features/music/components/music-card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="music-card">
+    <img :src="state.music.image" :alt="`${state.music.name}のサムネイル写真`">
+    <p class="name">{{ state.music.name }}</p>
+    <p class="artists">{{ state.music.artists }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const state = reactive({
+    music: {
+      image: 'https://i.kfs.io/album/global/162607085,0v1/fit/500x500.jpg',
+      name: 'きらり',
+      artists: '藤井風',
+    }
+  })
+</script>
+
+<style lang="scss">
+  .music-card {
+    width: 220px;
+    height: 335px;
+    padding: 25px 25px 0 25px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 12px 2px $music-card-shadow;
+
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
+
+    p {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    .name {
+      margin: 20px 0 0 -2px;
+      font-weight: 700;
+      font-size: 21px;
+      line-height: 25px;
+      color: $text-color-black;
+    }
+
+    .artists {
+      margin-top: 10px;
+      font-weight: 300;
+      font-size: 14px;
+      line-height: 17px;
+      color: $text-color-gray;
+    }
+  }
+</style>
