59de2462e7c1ecf2a91b6908bde592aff32bc090
txxxxc <tomoya113.code@gmail.com>
2023-03-15 01:01:19 +0900

    [add] RoomFooter.vue

---
+++ member/src/components/RoomFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <img width="60px" src="img/kansai-logo-white.svg">
+    <p>&copy; Life is Tech! Kansai Mentors</p>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: $gradient-orange;
+    padding: 32px 0;
+    p {
+      font-size: 20px;
+      color: $text-color-white;
+      margin-left: 16px;
+      vertical-align: middle;
+    }
+  }
+</style>
