137b53f2a1f3ef8bcd7b6dbe5c09f8576496cba6
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-15 04:07:40 +0900

    fix: textinput-css

--- member/src/components/TextInput.vue
+++ member/src/components/TextInput.vue
@@ -1,16 +1,17 @@
 <template>
-  <input class="search" type="text" placeholder="Search" autocomplete="on">
+  <input class="search" type="text" placeholder="曲名・アーティスト名で検索" autocomplete="on">
 </template>
 <script setup lang="ts">
 </script>
 <style lang="scss">
   .search {
     display: block;
+    width: 350px;
+    height: 20px;
     background: $background-color-textfield;
     color: $text-color-black;
-    border: 0;
-    padding: 10px 32px 10px 32px;
-    font-size: 20px;
+    border: none;
+    padding: 15px 40px;
     border-radius: 34px;
     &::placeholder {
       color: $text-color-placeholder;
