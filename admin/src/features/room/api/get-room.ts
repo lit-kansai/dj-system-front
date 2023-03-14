d17e6845364fb5f7aaa1793c3d2926183e5fffe3
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-15 01:10:23 +0900

    fix: get-room

--- admin/src/features/room/api/get-room.ts
+++ admin/src/features/room/api/get-room.ts
@@ -1,19 +1,91 @@
 import { z } from 'zod'
-import { _CamelizedAPIResponse } from '@dj-system/utils'
+import { CamelizedAPIResponse, toSchema } from '@dj-system/utils'
+import { ApiInstance } from '@dj-system/api-client'
 import { GetRequestOutput, apiClient } from '@/libs'
 
 export type GetRoomInput = { roomId: string }
-const responseSchema = z.object({
-  description: z.string(),
-  id: z.string(),
-  name: z.string()
-})
 
-export type GetRoomResponse = _CamelizedAPIResponse<z.infer<typeof responseSchema>>
+export type GetRoomResponse = CamelizedAPIResponse<ReturnType<ApiInstance['mc']['room']['_roomId']>['$get']>
+
+const usersSchema = toSchema<GetRoomResponse['users']>()(
+  z.object({
+    id: z.number(),
+    googleId: z.string(),
+    email: z.string(),
+    name: z.string(),
+    icon: z.string(),
+    isAdmin: z.boolean(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+    linkedProviders: z.object({
+      provider: z.string(),
+      isConnected: z.boolean(),
+    }).array().optional()
+  }).array()
+)
+
+const musicsSchema = toSchema<GetRoomResponse['musics']>()(
+  z.object({
+    id: z.number(),
+    providedMusicId: z.string(),
+    letterId: z.string().optional(),
+    artists: z.string(),
+    album: z.string(),
+    name: z.string(),
+    thumbnail: z.string(),
+    duration: z.string(),
+    letter: z.object({
+      id: z.string(),
+      roomId: z.number(),
+      radioName: z.string(),
+      message: z.string(),
+      createdAt: z.string(),
+      updatedAt: z.string()
+    })
+  }).array()
+)
+
+const lettersSchema = toSchema<GetRoomResponse['letters']>()(
+  z.object({
+    id: z.string(),
+    roomId: z.number(),
+    radioName: z.string(),
+    message: z.string(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+    musics: z.object({
+      id: z.number(),
+      providedMusicId: z.string(),
+      letterId: z.string().optional(),
+      artists: z.string(),
+      album: z.string(),
+      name: z.string(),
+      thumbnail: z.string(),
+      duration: z.string(),
+    }).array()
+  }).array()
+)
+
+const responseSchema = toSchema<GetRoomResponse>()(
+  z.object({
+    id: z.number(),
+    ownerUserId: z.number(),
+    displayId: z.string(),
+    name: z.string(),
+    description: z.string(),
+    playlistId: z.string().optional(),
+    provider: z.string().optional(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+    users: usersSchema,
+    musics: musicsSchema,
+    letters: lettersSchema,
+  })
+)
 
 export const getRoom = async (input: GetRoomInput): GetRequestOutput<GetRoomResponse> => {
   const result = await useAsyncData(async () => {
-    const response = await apiClient().room._roomId(input.roomId).$get()
+    const response = await apiClient().mc.room._roomId(input.roomId).$get()
 
     const parseResult = responseSchema.safeParse(response)
     if (!parseResult.success) {
