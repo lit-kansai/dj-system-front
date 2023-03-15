d34c5d0be7e8b6009d008b229b1a8f53b7ed9c48
Yo Higashida <57238213+penguin4731@users.noreply.github.com>
2023-03-16 01:46:47 +0900

    add: cooltime (#215)

--- member/src/pages/[id]/cooltime.vue
+++ member/src/pages/[id]/cooltime.vue
@@ -1,5 +1,36 @@
 <template>
-  <h1>room/[id]/cooltime.vue</h1>
+  <div class="cooltime">
+    <p>次のリクエストまでちょっと待ってね！</p>
+    <strong>05:00</strong>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
+<style scoped lang="scss">
+.cooltime {
+  width: calc(100vw - 160px);
+  height: calc(100vh - 250px);
+  background: $gradient-orange;
+  padding: 60px 80px 100px 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  p {
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 39px;
+    color: $text-color-white
+  }
+  strong {
+    font-weight: 500;
+    font-size: 64px;
+    line-height: 61px;
+    background-color: $background-color;
+    box-shadow: 0px 3px 6px $shadow-color;
+    padding: 40px 60px;
+    border-radius: 10px;
+  }
+}
+</style>
