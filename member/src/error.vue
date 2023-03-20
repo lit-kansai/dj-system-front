fb485f39bb375fb89b2227395d9088ca3b3f3f1d
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-21 02:09:58 +0900

    add: error-page

--- member/src/error.vue
+++ member/src/error.vue
@@ -20,9 +20,29 @@
 <style scoped lang="scss">
   .wrapper {
     display: flex;
+    justify-content: center;
+    align-items: center;
     height: calc(100vh - $app-bar-height);
+    margin: 0 auto;
     .container {
-
+      text-align: center;
+      padding-bottom: 70px;
+      h1, p {
+        font-family: 'Mukta';
+        font-weight: 700;
+      }
+      h1 {
+        display: inline-block;
+        font-size: 130px;
+        line-height: 130px;
+        letter-spacing: 0.1em;
+        background: $gradient-orange;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+      p {
+        font-size: 25px;
+      }
     }
   }
 </style>
