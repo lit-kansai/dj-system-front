41af8f933a3d17b8463b4e05c4f2dd8f08fcff58
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-21 01:57:31 +0900

    add: error

---
+++ member/src/error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <RoomHeader :is-show-search="false" />
+    <div class="wrapper">
+      <div class="container">
+        <h1>{{ error?.statusCode ?? '' }}</h1>
+        <p>{{ error?.message ?? '' }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const error = useError()
+  useHead({
+    style: [{ children: 'body{margin: 0}' }]
+  })
+</script>
+
+<style scoped lang="scss">
+  .wrapper {
+    display: flex;
+    height: calc(100vh - $app-bar-height);
+    .container {
+
+    }
+  }
+</style>
