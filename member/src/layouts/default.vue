8328ab0190babdfc0ab061e4b68589af189a252e
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-20 21:34:27 +0900

    update: isShowSearch

--- member/src/layouts/default.vue
+++ member/src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <room-header />
+    <room-header :is-show-search="state.header.isShowSearch" />
     <router-view />
     <room-footer v-if="!state.isHiddenFooter" />
   </div>
@@ -9,10 +9,20 @@
 <script setup lang="ts">
   const route = useRoute()
   const state = reactive({
-    isHiddenFooter: route.path.match('.*(requested|cooltime).*')
+    header: {
+      isShowSearch: false,
+    },
+    isHiddenFooter: false
   })
   useHead({
     style: [{ children: 'body{margin: 0}' }]
+  })
+
+  onMounted(() => {
+    if (!route.path.match('.*(requested|cooltime).*')) {
+      state.header.isShowSearch = true
+      state.isHiddenFooter = true
+    }
   })
 </script>
 
