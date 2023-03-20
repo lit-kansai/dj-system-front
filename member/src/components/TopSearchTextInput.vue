791f97741e27e74ea2e39041917aae9f4bc4d6d3
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-21 03:53:24 +0900

    add: index-view

---
+++ member/src/components/TopSearchTextInput.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <input
+      class="search"
+      type="text"
+      placeholder="曲名・アーティスト名で検索"
+      autocomplete="on"
+    >
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+  input {
+    box-sizing: content-box;
+    display: block;
+    width: 330px;
+    height: 20px;
+    background: $gradient-orange;
+    color: $text-color-white;
+    font-weight: 400px;
+    border: none;
+    padding: 16px 40px 14px 55px;
+    border-radius: 34px;
+    &::placeholder {
+      font-weight: 400px;
+      color: $text-color-white;
+    }
+  }
+  div {
+    position: relative;
+    &::before {
+      content: "";
+      background-color: $text-color-white;
+      mask: url(~/assets/img/search.svg);
+      position: absolute;
+      top: 15px;
+      left: 25px;
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      z-index: 900;
+    }
+  }
+</style>
--- member/src/pages/[id]/index.vue
+++ member/src/pages/[id]/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="wrapper">
+    <div class="header">
+      <div class="contents">
+        <h1>DJ Gassi</h1>
+        <p>自分の好きな曲をリクエストしよう！</p>
+        <TopSearchTextInput />
+      </div>
+      <img src="~/assets/img/logo.svg">
+    </div>
     <div class="top-musics">
       <MusicCard
         v-for="music in state.musics"
@@ -20,7 +28,6 @@
 
   const musicInit: GetTop50MusicsResponse = []
   const state = reactive({
-    loading: true,
     musics: musicInit,
   })
 
@@ -30,8 +37,7 @@
     }
     const result = await music.api.getTop50Musics(requestInput)
     result.execute()
-    if (!result.data.value) { state.loading = false; return }
-    state.musics = result.data.value
+    state.musics = result.data.value ?? []
   }
 
   onMounted(async () => {
@@ -42,12 +48,32 @@
 <style scoped lang="scss">
   .wrapper {
     margin: 0 auto;
+    .header {
+      display: flex;
+      justify-content: space-between;
+      padding: 130px 0;
+      .contents {
+        h1 {
+          font-weight: 500;
+          font-size: 95px;
+          line-height: 115px;
+          padding-bottom: 10px;
+        }
+        p {
+          font-weight: 700;
+          font-size: 23.5px;
+          line-height: 35px;
+          color: $text-color-gray;
+          padding-bottom: 40px;
+        }
+      }
+    }
     .top-musics {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     gap: 60px 15px;
-    margin: 110px auto;
+    margin-bottom: 110px;
   }
 }
 </style>
