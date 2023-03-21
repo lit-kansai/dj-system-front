62d6bf1c991c1dc32b3dceacceee509472035600
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-21 14:25:43 +0900

    update: search-nav-button-style

--- member/src/components/TopSearchTextInput.vue
+++ member/src/components/TopSearchTextInput.vue
@@ -6,30 +6,27 @@
       placeholder="曲名・アーティスト名で検索"
       autocomplete="on"
     >
+    <input type="submit" value="検索" @click="onclickSearch">
   </div>
 </template>
 <script setup lang="ts">
+  const onclickSearch = () => {
+    console.log('search')
+  }
 </script>
 
 <style scoped lang="scss">
-  input {
-    box-sizing: content-box;
-    display: block;
-    width: 330px;
-    height: 20px;
-    background: $gradient-orange;
-    color: $text-color-white;
-    font-weight: 400px;
-    border: none;
-    padding: 16px 40px 14px 55px;
-    border-radius: 34px;
-    &::placeholder {
-      font-weight: 400px;
-      color: $text-color-white;
-    }
-  }
   div {
+    width: 425px;
+    height: 50px;
+    padding: 3px !important;
+    border-radius: 40px;
+    background: $gradient-orange;
+    display: flex;
+    justify-content: left;
+    align-items: center;
     position: relative;
+    z-index: 1;
     &::before {
       content: "";
       background-color: $text-color-white;
@@ -41,6 +38,51 @@
       width: 20px;
       height: 20px;
       z-index: 900;
+    }
+    &:focus-within::before {
+      display: none;
+    }
+
+    input[type="text"] {
+      width: 100%;
+      height: 100%;
+      display: block;
+      background-color: transparent;
+      color: $text-color-white;
+      font-weight: 400;
+      border: none;
+      padding: 16px 80px 14px 55px;
+      border-radius: 40px;
+      outline: none;
+      transition: all 0.55s ease-in-out;
+      &::placeholder {
+        font-weight: 400;
+        color: $text-color-white;
+      }
+      &:focus {
+        width: calc(100% - 80px);
+        color: $text-color-black;
+        background-color: $background-color;
+        border-radius: 40px 0px 0px 40px;
+        padding-left: 25px;
+        &::placeholder {
+          color: $text-color-black;
+        }
+      }
+    }
+
+    input[type="submit"] {
+      width: 80px;
+      height: 100%;
+      margin-right: 3px;
+      padding: 0;
+      position: absolute;
+      right: 0px;
+      color: $text-color-white;
+      background-color: transparent;
+      border: none;
+      z-index: 999;
+      cursor: pointer;
     }
   }
 </style>
