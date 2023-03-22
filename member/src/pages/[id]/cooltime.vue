0cb87b87d1e4bea7b425aa3842bc74ea712dbaa0
Yo Higashida <y.higashida0226+app@gmail.com>
2023-03-22 14:43:44 +0900

    update: cooltime

--- member/src/pages/[id]/cooltime.vue
+++ member/src/pages/[id]/cooltime.vue
@@ -5,6 +5,9 @@
       <p>次のリクエストまでちょっと待ってね！</p>
       <strong>{{ state.displayTimer.min }}:{{ state.displayTimer.sec }}</strong>
     </div>
+    <div class="mentors">
+      <KansaiMentors />
+    </div>
   </div>
 </template>
 
@@ -29,38 +32,45 @@
 </script>
 
 <style scoped lang="scss">
-.cooltime {
-  width: 100vw;
-  height: calc(100vh - $app-bar-height);
-  background: $gradient-orange;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 20px;
-  @include tablet() {
-    padding: 60px 80px 100px 80px;
-    gap: 50px;
-  }
-  p {
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 19px;
-    color: $text-color-white;
-    text-align: center;
+  .cooltime {
+    width: 100vw;
+    height: calc(100vh - $app-bar-height);
+    background: $gradient-orange;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
     @include tablet() {
-      font-size: 32px;
-      line-height: 39px;
+      padding: 60px 80px 100px 80px;
+      gap: 50px;
+    }
+    p {
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 19px;
+      color: $text-color-white;
+      text-align: center;
+      @include tablet() {
+        font-size: 32px;
+        line-height: 39px;
+      }
+    }
+    strong {
+      font-weight: 500;
+      font-size: 64px;
+      line-height: 61px;
+      background-color: $background-color;
+      box-shadow: 0px 3px 6px $shadow-color;
+      padding: 40px 60px;
+      border-radius: 10px;
     }
   }
-  strong {
-    font-weight: 500;
-    font-size: 64px;
-    line-height: 61px;
-    background-color: $background-color;
-    box-shadow: 0px 3px 6px $shadow-color;
-    padding: 40px 60px;
-    border-radius: 10px;
+  .mentors {
+    width: 100vw;
+    position: fixed;
+    bottom: 10px;
+    display: flex;
+    justify-content: center;
   }
-}
 </style>
