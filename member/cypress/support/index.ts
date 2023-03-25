972d4077944a26db797c068fe3253662b8301c4b
txxxxc <tomoya113.code@gmail.com>
2023-03-25 20:27:02 +0900

    [add] support functions

--- member/cypress/support/index.ts
+++ member/cypress/support/index.ts
@@ -1,6 +1,16 @@
-Cypress.on('test:before:run:async', async () => {
-  if (!window.msw) {
-    const { worker } = await import('@/libs/msw')
-    await worker.start()
-  }
+const ID = 'sample-gassi'
+Cypress.Commands.add('visitRoomTopPage', () => {
+  cy.visit(`/${ID}`)
+})
+
+Cypress.Commands.add('visitRoomSearchResultPage', () => {
+  cy.visit(`/${ID}/search?q=YOASOBI`)
+})
+
+Cypress.Commands.add('visitCoolTimePage', () => {
+  cy.visit(`/${ID}/cooltime`)
+})
+
+Cypress.Commands.add('visitRequestedPage', () => {
+  cy.visit(`/${ID}/requested`)
 })
