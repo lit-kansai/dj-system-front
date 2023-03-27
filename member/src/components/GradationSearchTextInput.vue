c69a5ba6a535aa2fc3fd96cf4b8d8f570bd57943
txxxxc <tomoya113.code@gmail.com>
2023-03-27 23:55:02 +0900

    [update] GradationSearchTextInput

--- member/src/components/GradationSearchTextInput.vue
+++ member/src/components/GradationSearchTextInput.vue
@@ -84,7 +84,7 @@
       }
       &:focus {
         width: calc(100% - 80px);
-        color: $text-color-gray;
+        color: $text-color-black;
         background-color: $background-color;
         border-radius: 40px 0px 0px 40px;
         padding-left: 25px;
