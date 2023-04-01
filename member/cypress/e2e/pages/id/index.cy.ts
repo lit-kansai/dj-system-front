65da9dd53e9ce9a3d4864d6901f61b0101c68779
txxxxc <tomoya113.code@gmail.com>
2023-04-01 13:35:39 +0900

    [move] e2e tests

--- member/cypress/e2e/pages/id/index.cy.ts
+++ member/cypress/e2e/pages/id/index.cy.ts
@@ -1,5 +1,17 @@
 import { clearLocalStorage, setDefaultCooltime, setPreviousCooltime } from '@/utils'
 describe('/[id]/index.vue', () => {
+  it('ページにアクセスしたらTop50の楽曲が12曲表示されている', () => {
+    cy.visitRoomTopPage()
+    cy.wait(1000)
+    cy.get('[data-test-id=music-card]').should('have.length', 12)
+  })
+
+  it('表示されている楽曲を選択するとモーダルが表示され、楽曲の情報が表示されている', () => {
+  })
+
+  it('モーダルの送信ボタンを押すとリクエストが送信され、requestedに遷移する。またLocalStorageにはクールダウンに関するデータが入ってる', () => {
+  })
+
   it('localStorageにCooltimeが保存されている時にcooltimeに遷移する', () => {
     setDefaultCooltime()
     cy.visitRoomTopPage()
@@ -20,7 +32,6 @@ describe('/[id]/index.vue', () => {
   })
 
   it('localStorageに値が保存されていない時そのままページに残る', () => {
-    cy.visit('/')
     cy.visitRoomTopPage()
     cy.wait(1000)
     cy.location().should((location) => {
