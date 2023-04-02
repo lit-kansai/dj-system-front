6bfcfd525a752eda63ed93e98b367d528353cd4e
txxxxc <tomoya113.code@gmail.com>
2023-04-02 11:19:38 +0900

    [add] pages/id/index/cooltime-routing.cy.ts

---
+++ member/cypress/e2e/pages/id/index/cooltime-routing.cy.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai'
+import { clearLocalStorage, setDefaultCooltime, setPreviousCooltime } from '@/utils'
+describe('/[id]/index.vue', () => {
+  it('localStorageにCooltimeが保存されている時にcooltimeに遷移する', () => {
+    setDefaultCooltime()
+    cy.visitRoomTopPage()
+    cy.wait(1000)
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/sample-gassi/cooltime')
+    })
+    cy.contains('次のリクエストまでちょっと待ってね！')
+  })
+
+  it('localStorageに過去の時間が保存されている時そのままページに残る', () => {
+    setPreviousCooltime()
+    cy.visitRoomTopPage()
+    cy.wait(1000)
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/sample-gassi')
+    })
+  })
+
+  it('localStorageに値が保存されていない時そのままページに残る', () => {
+    cy.visitRoomTopPage()
+    cy.wait(1000)
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/sample-gassi')
+    })
+  })
+
+  afterEach(() => {
+    clearLocalStorage()
+  })
+})
