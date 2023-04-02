551cde8edad7f2f2fc0ad8f8b39b07214eed31d9
txxxxc <tomoya113.code@gmail.com>
2023-04-02 11:19:07 +0900

    [update] entry point

--- member/src/features/music/index.ts
+++ member/src/features/music/index.ts
@@ -1,8 +1,19 @@
 import { requestMusic, searchMusics, getTop50Musics } from './api'
+import {
+  getTop50MusicsHandler,
+  requestMusicHandler,
+  searchMusicsHandler
+} from './mock'
+
 export const music = {
   api: {
     requestMusic,
     searchMusics,
     getTop50Musics
+  },
+  msw: {
+    getTop50MusicsHandler,
+    requestMusicHandler,
+    searchMusicsHandler
   }
 }
--- member/src/features/room/index.ts
+++ member/src/features/room/index.ts
@@ -1,8 +1,12 @@
 import { getRoomOverview } from './api'
+import { roomOverviewResponseHandler } from './mock'
 export * from './types'
 export * from './composables'
 export const room = {
   api: {
     getRoomOverview
+  },
+  msw: {
+    roomOverviewResponseHandler
   }
 }
