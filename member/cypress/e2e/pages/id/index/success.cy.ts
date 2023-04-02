91d91efcb6b72715d476e48ccd2d9911354058b0
txxxxc <tomoya113.code@gmail.com>
2023-04-02 14:20:50 +0900

    [add] tests

--- member/cypress/e2e/pages/id/index/success.cy.ts
+++ member/cypress/e2e/pages/id/index/success.cy.ts
@@ -4,17 +4,19 @@ import { clearLocalStorage } from '@/utils'
 describe('/[id]/index.vue', () => {
   beforeEach(() => {
     cy.viewport(1920, 1080)
+    cy.visitRoomTopPage()
+    cy.wait(500)
+  })
+
+  afterEach(() => {
+    clearLocalStorage()
   })
 
   it('ページにアクセスしたらTop50の楽曲が12曲表示されている', () => {
-    cy.visitRoomTopPage()
-    cy.wait(500)
     cy.get('[data-test-id=music-card]').should('have.length', 12)
   })
 
   it('表示されている楽曲を選択するとモーダルが表示され、楽曲の情報が表示されている', () => {
-    cy.visitRoomTopPage()
-    cy.wait(500)
     cy.get('[data-test-id=music-card]').eq(1).click()
     cy.get('[data-test-id=modal-container]').should('exist')
     cy.get('[data-test-id=modal-music-name]').should('not.have.text', '')
@@ -24,8 +26,6 @@ describe('/[id]/index.vue', () => {
   })
 
   it('モーダルの送信ボタンを押すとリクエストが送信され、requestedに遷移する。またLocalStorageにはクールダウンに関するデータが入ってる', () => {
-    cy.visitRoomTopPage()
-    cy.wait(500)
     cy.get('[data-test-id=music-card]').eq(1).click()
     cy.get('[data-test-id=modal-submit-button]').click()
     cy.window().then((window) => {
@@ -39,7 +39,30 @@ describe('/[id]/index.vue', () => {
     })
   })
 
-  afterEach(() => {
-    clearLocalStorage()
+  it('検索バーに文字を入力してエンターを押した時、検索結果画面に遷移する', () => {
+    cy.get('[data-test-id=music-search-text-input]')
+      .click()
+      .find('input[type="text"]')
+      .type('YOASOBI{enter}')
+    cy.wait(500)
+    cy.location().should((location) => {
+      expect(location.search).to.eq('?q=YOASOBI')
+      expect(location.pathname).to.eq('/sample-gassi/search')
+    })
+  })
+
+  it('検索バーに文字を入力して検索ボタンを押した時、検索結果画面に遷移する', () => {
+    cy.get('[data-test-id=music-search-text-input]')
+      .click()
+      .find('input[type="text"]')
+      .type('YOASOBI')
+    cy.get('[data-test-id=music-search-text-input]')
+      .find('input[type="submit"]')
+      .click()
+    cy.wait(500)
+    cy.location().should((location) => {
+      expect(location.search).to.eq('?q=YOASOBI')
+      expect(location.pathname).to.eq('/sample-gassi/search')
+    })
   })
 })
