518d1be00603344092df0fee06b469d9e92f87b6
Tomoya Tanaka <tt.ios.develop@gmail.com>
2023-04-03 15:24:38 +0900

    Feature/#283 integration tests (#286)
    * [add] data-test-id to RoomHeader
    
    * [add] search page integration test
    
    * [add] cooltime page integration test
    
    * [add] requested page integration test
    
    * [add] index page integration test

--- member/cypress/e2e/pages/id/requested.cy.ts
+++ member/cypress/e2e/pages/id/requested.cy.ts
@@ -1,5 +1,8 @@
 import { clearLocalStorage, setDefaultCooltime, setPreviousCooltime } from '@/utils'
 describe('/[id]/requested.vue', () => {
+  beforeEach(() => {
+    cy.clock(new Date().getTime())
+  })
   it('localStorageにCooltimeが保存されている時にcooltimeに遷移する', () => {
     setDefaultCooltime()
     cy.visitRequestedPage()
@@ -25,6 +28,21 @@ describe('/[id]/requested.vue', () => {
     cy.wait(1000)
     cy.location().should((location) => {
       expect(location.pathname).to.eq('/sample-gassi')
+    })
+  })
+
+  it('クールタイム終わったらトップページに画面遷移する', () => {
+    cy.viewport(1920, 1080)
+    cy.visitRoomTopPage()
+    cy.get('[data-test-id=music-card]').eq(1).click()
+    cy.get('[data-test-id=modal-submit-button]').click()
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/sample-gassi/requested')
+    })
+    // 1分
+    cy.tick(1000 * 60)
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/sample-gassi/cooltime')
     })
   })
 
--- member/cypress/e2e/pages/id/search/cooltime-routing.cy.ts
+++ member/cypress/e2e/pages/id/search/cooltime-routing.cy.ts
@@ -32,3 +32,4 @@ describe('/[id]/search.vue', () => {
     clearLocalStorage()
   })
 })
+
