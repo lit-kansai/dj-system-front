d8a6c9c6eceb9a5bf54b6869ab5be560aa95c2cd
Yo Higashida <y.higashida0226+app@gmail.com>
2023-05-02 04:15:19 +0900

    delete: console.log

--- admin/src/constants/external-url.ts
+++ admin/src/constants/external-url.ts
@@ -6,6 +6,5 @@ export const MEMBER_SITE_URL = () => useRuntimeConfig().public.MEMBER_SITE_URL
 
 export const MEMBER_REQUEST_URL = (roomId: string) => {
   const { MEMBER_SITE_URL } = useRuntimeConfig().public
-  console.log(useRuntimeConfig().public)
   return `${MEMBER_SITE_URL}/${roomId}`
 }
