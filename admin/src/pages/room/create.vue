1179855df341183bfaee7297aed7d60ef6009e66
Yo Higashida <y.higashida0226+app@gmail.com>
2023-05-02 18:43:54 +0900

    fix: rules

--- admin/src/pages/room/create.vue
+++ admin/src/pages/room/create.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="create">
-    <p class="text-h5 text-weight-bold q-mb-sm">
-      ルームを作成する
-    </p>
-    <q-input
-      v-model="state.form.roomName"
-      outlined
-      label="ルームネーム*"
-      :rules="[(val) => !!val || 'Field is required']"
-    />
-    <q-input v-model="state.form.description" outlined label="ルーム説明" class="not-rule-input" />
-    <q-select
-      v-model="state.form.provider"
-      outlined
-      :options="state.providers"
-      label="外部サービス*"
-      class="not-rule-input"
-    />
-    <q-input
-      v-model="state.form.urlName"
-      outlined
-      label="リクエストURL*"
-      class="url-prefix"
-      :prefix="`${MEMBER_SITE_URL()}/`"
-      :rules="[(val) => !!val || 'Field is required']"
-    />
-    <div class="row justify-end items-end">
-      <q-btn color="grey-1" text-color="dark" class="q-mr-sm" label="キャンセル" @click="onClickCancel" />
-      <q-btn color="primary" label="ルームを作成する" @click="onClickCreateButton" />
-    </div>
+  <div>
+    <q-form class="create" @submit="onClickCreateButton" @reset="onClickReset">
+      <p class="text-h5 text-weight-bold q-mb-sm">
+        ルームを作成する
+      </p>
+      <q-input
+        v-model="state.form.roomName"
+        outlined
+        label="ルームネーム*"
+        :rules="[(val) => !!val || 'ルームネームを入力してください']"
+      />
+      <q-input
+        v-model="state.form.description"
+        outlined
+        label="メモ（ルーム説明）*"
+        class="not-rule-input"
+        :rules="[(val) => !!val || 'メモ（ルーム説明）を入力してください']"
+      />
+      <q-select
+        v-model="state.form.provider"
+        outlined
+        :options="state.providers"
+        label="外部サービス*"
+        class="not-rule-input"
+        :rules="[(val) => !!val || '使用する外部サービスを選択してください']"
+      />
+      <q-input
+        v-model="state.form.urlName"
+        outlined
+        label="リクエストURL*"
+        class="url-prefix"
+        :prefix="`${MEMBER_SITE_URL}/`"
+        :rules="[(val) => !!val || 'リクエストURLを入力してください']"
+      />
+      <div class="row justify-start">
+        <q-btn color="primary" type="submit" class="q-mr-sm" label="ルームを作成する" />
+        <q-btn color="primary" type="reset" flat label="リセット" />
+      </div>
+    </q-form>
     <q-dialog v-model="confirmProviders" persistent>
       <q-card>
         <q-card-section>
@@ -56,9 +65,9 @@
 
 <script setup lang="ts">
   import { room } from '@/features'
-  import { MEMBER_SITE_URL } from '@/constants'
 
-  // const $q = useQuasar()
+  // const $q = quasar()
+  const { MEMBER_SITE_URL } = useRuntimeConfig().public
   const router = useRouter()
   const userState = useUserState()
   const state = reactive({
@@ -88,8 +97,11 @@
     router.push(`/room/${data.value.displayId}`)
   }
 
-  const onClickCancel = () => {
-    router.go(-1)
+  const onClickReset = () => {
+    state.form.urlName = ''
+    state.form.roomName = ''
+    state.form.description = ''
+    state.form.provider = ''
   }
 
 </script>
