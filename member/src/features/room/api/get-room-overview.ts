88e55d9d8cd879dec66c9e8f2a65aac23ec184b6
txxxxc <tomoya113.code@gmail.com>
2023-07-03 16:36:49 +0900

    update: api/get-room-overview

--- member/src/features/room/api/get-room-overview.ts
+++ member/src/features/room/api/get-room-overview.ts
@@ -13,7 +13,7 @@ export type GetRoomResponse = _CamelizedAPIResponse<z.infer<typeof responseSchem
 
 export const getRoomOverview = async (input: GetRoomInput): GetRequestOutput<GetRoomResponse> => {
   const result = await useAsyncData(async () => {
-    const response = await apiClient().room._room_id(input.roomId).$get()
+    const response = await apiClient().room._roomId(input.roomId).$get()
 
     const parseResult = responseSchema.safeParse(response)
     if (!parseResult.success) {
