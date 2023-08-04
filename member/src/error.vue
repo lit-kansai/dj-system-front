b3a88d60f0d3169b368b724486bc7d4bd3c59378
txxxxc <tomoya113.code@gmail.com>
2023-08-04 12:21:35 +0900

    update: error.vue

--- member/src/error.vue
+++ member/src/error.vue
@@ -3,15 +3,55 @@
     <RoomHeader :is-show-search="false" />
     <div class="wrapper">
       <div class="container">
-        <h1>{{ error?.statusCode ?? '' }}</h1>
-        <p>{{ error?.message ?? '' }}</p>
+        <h1 v-if="statusCode.length" class="gradient-title">{{ statusCode }}</h1>
+        <h1 v-if="!statusCode.length" class="normal-title">エラーが発生しました</h1>
+        <p class="error-overview">{{ errorOverview }}</p>
+        <GradientButton class="button-container" title="元のページに戻る" @click.native="reloadPage()" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  const error = useError()
+  import { handleAPIClientError } from './libs'
+  type AppError = {
+    url: string;
+    statusCode: number;
+    statusMessage: string;
+    message: string;
+    description: string;
+    data?: unknown;
+  }
+
+  const isAppError = (arg: unknown): arg is AppError => {
+    return arg != null && typeof arg === 'object' && 'statusCode' in arg
+  }
+  const statusCode = ref('')
+  const errorOverview = ref('')
+  const stack = ref('')
+  const error = useError().value
+  const reloadPage = () => {
+    window.location.reload()
+  }
+  onMounted(() => {
+    if (error && isAppError(error) && error.data instanceof Error) {
+      handleAPIClientError(error.data, {
+        onAxiosError: (axiosError) => {
+          if (axiosError.response) { statusCode.value = String(axiosError.response?.status) }
+          errorOverview.value = axiosError.message
+          stack.value = axiosError.stack ?? ''
+        },
+        onZodError: (zodError) => {
+          errorOverview.value = 'レスポンスのパース中にエラーが発生しました。'
+          stack.value = zodError.stack ?? ''
+        },
+        onUnexpectedError: (unexpectedError) => {
+          errorOverview.value = '予期せぬエラーが発生しました。'
+          stack.value = unexpectedError.stack ?? ''
+        }
+      })
+    }
+  })
   useHead({
     style: [{ children: 'body{margin: 0}' }]
   })
@@ -22,26 +62,32 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     height: calc(100dvh - $app-bar-height);
     .container {
       text-align: center;
-      padding-bottom: 70px;
-      h1, p {
-        font-family: $font-english;
-        font-weight: 700;
-      }
-      h1 {
-        display: inline-block;
-        font-size: 130px;
-        line-height: 130px;
-        letter-spacing: 0.1em;
-        background: $color-gradient-orange;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-      }
-      p {
-        font-size: 25px;
-      }
     }
+  }
+  .normal-title {
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 1.5;
+  }
+  .gradient-title {
+    font-family: $font-english;
+    font-weight: 700;
+    display: inline-block;
+    font-size: 130px;
+    line-height: 130px;
+    letter-spacing: 0.1em;
+    background: $color-gradient-orange;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  .error-overview {
+    font-weight: 600;
+  }
+  .button-container {
+    margin-top: 24px;
   }
 </style>
