d2778025d6b4e97de238c1299395b37b3e0224d2
Yo Higashida <y.higashida0226+app@gmail.com>
2023-11-20 16:51:46 +0900

    fix: feedback-link

--- admin/src/layouts/default.vue
+++ admin/src/layouts/default.vue
@@ -8,25 +8,41 @@
           <div class="button">
             <q-btn dense round @click="toggleProfileOpen">
               <q-avatar size="36px">
-                <img :src="state.user.image">
+                <img :src="state.user.image" />
               </q-avatar>
             </q-btn>
             <div class="toggle">
-              <svg v-if="!state.profileOpen" viewBox="0 0 8 6" fill="black" xmlns="http://www.w3.org/2000/svg" class="fill-current"><path d="M4.21053 5.05263L8 0H0.421053L4.21053 5.05263Z" /></svg>
-              <svg v-else viewBox="0 0 8 6" fill="black" xmlns="http://www.w3.org/2000/svg" class="fill-current rotate-180"><path d="M4.21053 5.05263L8 0H0.421053L4.21053 5.05263Z" /></svg>
+              <svg
+                v-if="!state.profileOpen"
+                viewBox="0 0 8 6"
+                fill="black"
+                xmlns="http://www.w3.org/2000/svg"
+                class="fill-current"
+              >
+                <path d="M4.21053 5.05263L8 0H0.421053L4.21053 5.05263Z" />
+              </svg>
+              <svg
+                v-else
+                viewBox="0 0 8 6"
+                fill="black"
+                xmlns="http://www.w3.org/2000/svg"
+                class="fill-current rotate-180"
+              >
+                <path d="M4.21053 5.05263L8 0H0.421053L4.21053 5.05263Z" />
+              </svg>
             </div>
           </div>
           <div v-if="state.profileOpen" class="menu">
             <div class="content">
               <q-avatar size="42px">
-                <img :src="state.user.image">
+                <img :src="state.user.image" />
               </q-avatar>
               <div class="q-ml-sm">
                 <p class="text-weight-bold">{{ state.user.name }}</p>
                 <p>{{ state.user.email }}</p>
               </div>
             </div>
-            <hr>
+            <hr />
             <q-btn flat align="left" class="q-pl-none width-max" @click="logout">Logout</q-btn>
           </div>
         </div>
@@ -61,7 +77,7 @@
           <q-btn flat align="left" class="q-px-lg q-py-md full-width" to="/settings">
             <q-icon name="settings" size="1.6em" class="q-mr-sm" />外部サービス連携
           </q-btn>
-          <q-btn flat align="left" class="q-px-lg q-py-md full-width" to="https://forms.gle/AbUwewkPuLWJjgrL8">
+          <q-btn flat align="left" class="q-px-lg q-py-md full-width" href="https://forms.gle/AbUwewkPuLWJjgrL8" target="_blank">
             <q-icon name="reviews" size="1.6em" class="q-mr-sm" />フィードバックを送信
           </q-btn>
         </div>
@@ -77,91 +93,91 @@
 </template>
 
 <script setup lang="ts">
-  import { auth } from '@/features'
-  import { LOGIN_PAGE } from '@/constants'
+import { auth } from "@/features";
+import { LOGIN_PAGE } from "@/constants";
 
-  const { rooms } = useRoomsState()
-  const userState = useUserState()
-  const state = reactive({
-    leftDrawerOpen: false,
-    profileOpen: false,
-    user: {
-      image: userState.state.value?.icon,
-      email: userState.state.value?.email,
-      name: userState.state.value?.name
-    }
-  })
-  const toggleLeftDrawer = () => {
-    state.leftDrawerOpen = !state.leftDrawerOpen
-  }
-  const toggleProfileOpen = () => {
-    state.profileOpen = !state.profileOpen
-  }
-  const logout = () => {
-    auth.api.logout()
-    navigateTo(LOGIN_PAGE)
-  }
+const { rooms } = useRoomsState();
+const userState = useUserState();
+const state = reactive({
+  leftDrawerOpen: false,
+  profileOpen: false,
+  user: {
+    image: userState.state.value?.icon,
+    email: userState.state.value?.email,
+    name: userState.state.value?.name,
+  },
+});
+const toggleLeftDrawer = () => {
+  state.leftDrawerOpen = !state.leftDrawerOpen;
+};
+const toggleProfileOpen = () => {
+  state.profileOpen = !state.profileOpen;
+};
+const logout = () => {
+  auth.api.logout();
+  navigateTo(LOGIN_PAGE);
+};
 </script>
 
 <style scoped lang="scss">
-  .container {
-    max-width: 1024px;
-    margin: 0 12px;
+.container {
+  max-width: 1024px;
+  margin: 0 12px;
 
-    @media (min-width: $breakpoint-md-min) {
-      margin: 0 auto;
-      padding-right: 24px;
-      padding-left: 24px;
+  @media (min-width: $breakpoint-md-min) {
+    margin: 0 auto;
+    padding-right: 24px;
+    padding-left: 24px;
+  }
+}
+.profile {
+  position: relative;
+
+  .button {
+    display: flex;
+    align-items: center;
+
+    .toggle {
+      width: 9px;
+      height: 9px;
+      margin-left: 8px;
+      margin-bottom: 12px;
     }
   }
-  .profile {
-    position: relative;
 
-    .button {
+  .menu {
+    position: absolute;
+    right: 0;
+    width: 310px;
+    margin-top: 4px;
+    padding: 20px 20px 13px 20px;
+    box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.2);
+    border-radius: 1rem;
+    background-color: white;
+    z-index: 30;
+
+    hr {
+      margin: 15px 0 5px 0;
+    }
+
+    .content {
       display: flex;
       align-items: center;
 
-        .toggle {
-          width: 9px;
-          height: 9px;
-          margin-left: 8px;
-          margin-bottom: 12px;
-        }
+      img {
+        width: 2rem;
+        height: 2rem;
+        object-fit: cover;
       }
 
-    .menu {
-      position: absolute;
-      right: 0;
-      width: 310px;
-      margin-top: 4px;
-      padding: 20px 20px 13px 20px;
-      box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, .2);
-      border-radius: 1rem;
-      background-color: white;
-      z-index: 30;
-
-      hr {
-        margin: 15px 0 5px 0;
+      p {
+        margin: 0;
       }
 
-      .content{
-        display: flex;
-        align-items: center;
-
-        img {
-          width: 2rem;
-          height: 2rem;
-          object-fit: cover;
-        }
-
-        p {
-          margin: 0;
-        }
-
-        :global(.width-max) {
-          width: 100%;
-        }
+      :global(.width-max) {
+        width: 100%;
       }
     }
   }
+}
 </style>
